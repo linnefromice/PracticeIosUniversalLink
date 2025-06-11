a8eb4054580f3161a21d02b44cb3b0fb9a782818
linnefromice <linnefromice@gmail.com>
2025-06-11 15:08:21 +0900

    initialize

---
+++ PracticeIosUniversalLink/PracticeIosUniversalLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeIosUniversalLinkApp.swift
+//  PracticeIosUniversalLink
+//
+//  Created by arata.haruyama on 2025/06/11.
+//
+
+import SwiftUI
+
+@main
+struct PracticeIosUniversalLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
